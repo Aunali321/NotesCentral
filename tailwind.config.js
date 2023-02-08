abc7f53d9dfc1f189e5b298db1b43d6ef187d98f
Aunali321 <aunvakil.aa@gmail.com>
2023-02-08 15:39:51 +0530

    feat: add illustration and pattern.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "media",
   content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [require("flowbite/plugin"), require("daisyui")],
+  daisyui: {
+    themes: ["light", "night"],
+    darkTheme: "night",
+  },
 };
