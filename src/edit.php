37ac48aa2c524f724cbb1869365db8c03b8029d6
Aunali321 <aunvakil.aa@gmail.com>
2023-02-17 00:36:16 +0530

    feat: add minor validation for code, output fields

--- src/edit.php
+++ src/edit.php
@@ -91,11 +91,11 @@ if (isset($_POST['edit'])) {
                 </div>
 
                 <label for="code" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Practical code</label>
-                <textarea id="code" rows="4" name="code" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write code..."><?= $practical['code'] ?></textarea>
+                <textarea id="code" rows="4" name="code" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write code..."><?= htmlspecialchars($practical['code']) ?></textarea>
 
 
                 <label for="output" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your output</label>
-                <textarea id="output" rows="4" name="output" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write output..."><?= $practical['output'] ?></textarea>
+                <textarea id="output" rows="4" name="output" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write output..."><?= htmlspecialchars($practical['output']) ?></textarea>
 
 
                 <button type="submit" name="edit" class="md:mx-auto md:w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
